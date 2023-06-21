9b45631f3a21c9327fb27fc5248b14a4e299b31e
Alexanderjsv <alexandersv2003@gmail.com>
2023-06-21 16:27:42 -0500

    Aaah

--- Calcu punteros/punterocalcu.h
+++ Calcu punteros/punterocalcu.h
@@ -5,3 +5,4 @@ void resta(float* p1,float* p2,float* r);
 void multi(float* p1,float* p2,float* r);
 void div(float* p1,float* p2,float* r);
 void sumaescalar(int n, int m, int* matriz, int* mult);
+void compararcad(char *cadena1,char *cadena2);
